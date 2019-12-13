87987a329daf8798b22196029624c2ef9d375c7f
JChartier001 <JChartier001@gmail.com>
2019-12-13 10:38:23 -0600

    configured server file

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const Projects = require('../projects/project-router.js');
+const Tasks = require('../tasks/tasks.js');
+const Resources = require('../resources/resources-router.js');
+
+const server = express();
+
+
+server.use(express.json());
+
+server.use('/api/projects', Projects);
+server.use('/api/tasks', Tasks);
+server.use('/api/resources', Resources);
+
+module.exports = server;
